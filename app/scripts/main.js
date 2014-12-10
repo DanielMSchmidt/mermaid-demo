30f862fb24c488b0a998eed1fd8f164889d8c976
Daniel Schmidt <dschmidt@weluse.de>
2014-12-10 22:18:54 +0100

    finished alpha

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,17 @@
-console.log('\'Allo \'Allo!');
+'use strict';
+
+$(function() {
+  $('.js-mermaid').each(function(i, el) {
+    var $el = $(el),
+        $content = $el.find('.js-mermaid-content'),
+        $target = $el.find('.js-mermaid-target');
+
+    $el.find('.js-mermaid-build').on('click', function(e) {
+      e.preventDefault();
+
+      var newGraphDefinition = $('<div class="mermaid">').text($content.val());
+      $target.empty().append(newGraphDefinition);
+      window.mermaid.init();
+    });
+  });
+});
